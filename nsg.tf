2a291e41763d31e1e722cd5e8b74cb43b527bd43
rakall <rakall@hotmail.es>
2021-04-07 15:00:47 +0200

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -1,20 +1,3 @@
-variable "idFrontEndSbn" {
-  type        = string
-}
-variable "idBackEndSbn" {
-  type        = string
-}
-variable "idmanagerSbn" {
-  type        = string
-}
-variable "loc"{
-    type        = string
-}
-
-variable "sbw10" {
-  type        = string
-}
-
 resource "azurerm_resource_group" "rg-nsg" {
   name = "rgt-nsg"
   location = var.loc
@@ -40,29 +23,4 @@ resource "azurerm_network_security_group" "nsg" {
   tags = {
     environment = "Labs"
   }
-}
-
-resource "azurerm_subnet_network_security_group_association" "vinculo1" {
-  subnet_id                 = var.idFrontEndSbn
-  network_security_group_id = azurerm_resource_group.rg-nsg.id
-}
-resource "azurerm_subnet_network_security_group_association" "vinculo2" {
-  subnet_id                 = var.idmanagerSbn
-  network_security_group_id = azurerm_resource_group.rg-nsg.id
-}
-resource "azurerm_subnet_network_security_group_association" "vinculo3" {
-  subnet_id                 = var.idBackEndSbn
-  network_security_group_id = azurerm_resource_group.rg-nsg.id
-}
-
-resource "azurerm_subnet_network_security_group_association" "vinculo4" {
-  subnet_id                 = var.sbw10
-  network_security_group_id = azurerm_resource_group.rg-nsg.id
-}
-
-output "gruposeguridad" {
-  value = azurerm_network_security_group.nsg.id
-}
-output "rgnsg" {
-  value = azurerm_resource_group.rg-nsg.name
 }
