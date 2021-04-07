5dc5cdaa638e56ce77795a991ce4d395e76256a9
rakall <rakall@hotmail.es>
2021-04-07 15:01:47 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,4 @@
-/*
-variable "resource_group" {
-  type        = string
-  description = "Resource Group to deploy NVA"
-}
-
-*/
-variable "subscription_id" {
-  type        = string
-  description = "Suscripcion sobre la que se va a trabajar"
- }
-
 variable "loc" {
   type        = string
   description = "Ubicacion"
   default ="westeurope"
-}
-variable "FrontEndSubnetprefix" {
-  type        = any
-  default     = "172.16.0.0/25"
-}
-variable "BackEndSubnetprefix" {
-  type        = any
-  default     = "172.16.0.128/25"
-}
-variable "pass"{
-    type        = string
-}
-variable "w10Subnetprefix" {
-  type        = any
-  default     = "172.16.0.128/25"
-}
